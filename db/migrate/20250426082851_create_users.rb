e7124d5ae897b3ce518c1c5f32fbe6cf26bb3e2f
idhruvnaik <dhruv97naik@gmail.com>
2025-04-26 15:14:49 +0530

    User Model : Creation

---
+++ db/migrate/20250426082851_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[7.2]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email, null: false, index: { unique: true }
+      t.string :password_digest, null: false, index: { unique: true }
+      t.string :auth_token, null: false, index: { unique: true }
+
+      t.timestamps
+    end
+  end
+end
